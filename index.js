6d7b6920dadbba7d115c6ff8f0eecf51fcc89f5b
envyusone <abriche123@gmail.com>
2026-02-18 19:25:33 -0700

    can enter club

--- index.js
+++ index.js
@@ -167,7 +167,9 @@ export function makeSLS(str1, str2) {
  * canEnterClub(5, 5); // 1
  */
 export function canEnterClub(you, date) {
-  /* TODO */
+  if (you <= 2 || date <= 2) return 0;
+  if (you >= 8 || date >= 8) return 2;
+  return 1;
 }
 
 /**
